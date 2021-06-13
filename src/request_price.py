d1cdaefaa629f0334fe04132608524a6d0a77b54
Rodrigo Jara <rodrigoagustinjara@gmail.com>
2021-06-13 12:43:37 +0200

    change token as a variable and not a file

--- src/request_price.py
+++ src/request_price.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python  
 import requests
+from token import TOKEN
 
 # get authorization code
-with open('data/token.txt') as file: token = file.read()
 url = 'https://api.estadisticasbcra.com/usd'
-headers = {"Authorization": f"Bearer {token}"}
+headers = {"Authorization": f"Bearer {TOKEN}"}
 
-r = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers)
 
-print(f'RESPONSE: {r.status_code}')
+print(f'RESPONSE: {response.status_code}')
 
-# extract the dates and values 
-values = [dv['v'] for dv in r.json()]
-dates = [dv['d'] for dv in r.json()]
+# # extract the dates and values 
+# values = [dv['v'] for dv in r.json()]
+# dates = [dv['d'] for dv in r.json()]
 
-dates_t = []
-for dv in r.json():
-    y, m, d = dv['d'].split('-')
-    dates_t.append((y, m, d))
+# dates_t = []
+# for dv in r.json():
+#     y, m, d = dv['d'].split('-')
+#     dates_t.append((y, m, d))
 
 
 
